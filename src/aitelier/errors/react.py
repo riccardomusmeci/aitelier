144c63b52ced4ef1031e5b8ebf24bc4bedb7b909
Riccardo Musmeci <riccardomusmeci92@gmail.com>
2025-02-02 17:16:17 +0100

    improved react agent

--- src/aitelier/errors/react.py
+++ src/aitelier/errors/react.py
@@ -8,7 +8,7 @@ class ReActParsingToolError(Exception):
 While parsing tool got the error "ParsingToolError".
 Your previous answer was: {response}
 You have to be sure that the your answer is provided in the correct format, like in this example: 
-    Act: <tool>tool_name</tool> <args> {{'arg1': value1, 'arg2': value2, ...}} </args> {stop_word if stop_word else ""}
+    <state>Act</state><content><tool>tool_name</tool> <args> {{'arg1': value1, 'arg2': value2, ...}} </args></content> {stop_word if stop_word else ""}
 Also always remember to put the dictionary key name between single quotes, like in this example: {{'arg1': value1, 'arg2': value2, ...}}. 
 Finally, remember to respect the data type in the args dictionary, for instance if value1 is a string, it must be enclosed in double quotes, if value2 is a list, it must be enclosed in square brackets, etc."""
 
